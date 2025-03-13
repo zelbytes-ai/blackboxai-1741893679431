b0115e9fc7dd2266feb0cd6d5e300b08780421c4
zelbytes-ai <zelbytesonline@gmail.com>
2025-03-14 00:51:51 +0530

    Add zel-app/frontend/src/index.js 1741893710502

---
+++ zel-app/frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import App from './App';
+import { ThemeProvider } from './contexts/ThemeContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
